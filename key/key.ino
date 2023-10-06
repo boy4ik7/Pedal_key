f3fffeb9dc75137268af163bc29b1bd777465846
BOY4ik <90923847+boy4ik7@users.noreply.github.com>
2023-10-06 23:37:09 +0300

    Update and rename key.ino to key/key.ino

--- key/key.ino
+++ key/key.ino
@@ -1,4 +1,3 @@
-#define led_pin PB0   // Пин для светодиода
 #define button PB2 // Пин кнопки
 
 #include "DigiKeyboard.h"
@@ -6,22 +5,19 @@ bool press = false;
 
 void setup() {
   pinMode(button, INPUT);
-  pinMode(led_pin, OUTPUT);
   DigiKeyboard.enableLEDFeedback();
 }
-+++++++++++++++++++++++++++++++++++++++++++++++
+
 void loop() {
   if (digitalRead(button) == HIGH) {
     if (press == false) {
       press = true;
       DigiKeyboard.sendKeyPress(KEYPAD_PLUS); //KEY_RIGHT_ALT KEYPAD_PLUS MODIFIERKEY_RIGHT_ALT
-      digitalWrite(led_pin, HIGH);
     } // else DigiKeyboard.delay(500);
   } else {
     if (press == true) {
       press = false;
       DigiKeyboard.sendKeyPress(0);
-      digitalWrite(led_pin, LOW);
     }
   }
 }
